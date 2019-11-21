841650891373cccb626d5ed273b750cafb777e0f
Max Wang <max@covar.com>
2019-11-21 17:50:45 -0500

    adding hot reloading

---
+++ webpack.dev.js
@@ -0,0 +1,24 @@
+const merge = require('webpack-merge');
+const path = require('path');
+const webpack = require('webpack');
+const common = require('./webpack.config');
+
+module.exports = merge(common, {
+  target: 'electron-renderer',
+  mode: 'development',
+  devtool: 'cheap-module-eval-source-map',
+  entry: ['webpack/hot/dev-server', './ui/index.jsx'],
+  output: {
+    path: path.join(__dirname, 'build_ui'),
+    filename: 'bundle.js',
+    publicPath: 'http://localhost:8080/build_ui',
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'build_ui'),
+    publicPath: 'http://localhost:8080/build_ui',
+  },
+  plugins: [
+    new webpack.IgnorePlugin(new RegExp('^(fs|ipc)$')),
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+});
