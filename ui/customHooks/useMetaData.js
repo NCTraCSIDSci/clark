da79bc9ea55f5cf3642caabc6b32625a6ddba6a9
Max Wang <max@covar.com>
2019-12-09 14:26:27 -0500

    adding patient and date picker to structured data setup

--- ui/customHooks/useMetaData.js
+++ ui/customHooks/useMetaData.js
@@ -4,40 +4,71 @@ import { cloneDeep } from 'lodash';
 import API from '../API';
 
 const initialMetaData = {
+  patient: {},
   labs: {},
   meds: {},
   vitals: {},
 };
 
 const initialFilter = {
+  patient: {},
   labs: {
     text: '',
     sort: 'desc',
     features: ['max', 'min', 'recent', 'oldest'],
-    count: 0,
   },
   meds: {
     text: '',
     sort: 'desc',
     features: ['count', 'boolean'],
-    count: 0,
   },
   vitals: {
     text: '',
     sort: 'desc',
     features: ['max', 'min', 'recent', 'oldest'],
-    count: 0,
   },
 };
+
+const patientMetaData = [
+  {
+    display: 'Age',
+  },
+  {
+    display: 'Gender',
+  },
+  {
+    display: 'Race',
+  },
+  {
+    display: 'Ethnicity',
+  },
+  {
+    display: 'Marital Status',
+  },
+];
 
 function useMetaData() {
   const [metaData, setMetaData] = useState(initialMetaData);
   const [initialLists, setInitialLists] = useState({});
+  const [initialized, setInitialized] = useState(false);
   const [filteredList, setFilteredList] = useState([]);
   const [loading, setLoading] = useState(true);
   const [filter, setFilter] = useState(initialFilter);
   const [tab, setTab] = useState(Object.keys(initialFilter)[0]);
   const [badgeNum, updateBadgeNum] = useState(0);
+
+  function updateDate(d) {
+    const tempMetaData = cloneDeep(metaData);
+    if (d) {
+      tempMetaData.patient.date = d;
+    } else { // null date, need to clear the selected metaData
+      delete tempMetaData.patient.date;
+      const len = tempMetaData.patient.Age ? tempMetaData.patient.Age.length : 0;
+      updateBadgeNum((prev) => prev - len);
+      delete tempMetaData.patient.Age;
+    }
+    setMetaData(tempMetaData);
+  }
 
   function updateFilteredList() {
     const tempFilteredList = initialLists[tab].filter((patient) => {
@@ -56,17 +87,19 @@ function useMetaData() {
     setFilteredList(tempFilteredList);
   }
 
-  function initialize() {
+  function initialize(type) {
     setLoading(true);
-    const calls = [API.getLabs(), API.getMeds(), API.getVitals()];
+    const calls = [API.getLabs(type), API.getMeds(type), API.getVitals(type)];
     Promise.all(calls)
       .then((res) => {
         setInitialLists({
           labs: res[0].data,
           meds: res[1].data,
           vitals: res[2].data,
+          patient: patientMetaData,
         });
         setLoading(false);
+        setInitialized(true);
       })
       .catch((err) => {
         console.log('error', err);
@@ -74,7 +107,7 @@ function useMetaData() {
   }
 
   function updateMetaData(code, system, value) {
-    const key = `${code} ${system}`;
+    const key = `${code}${system ? ` ${system}` : ''}`;
     if (metaData[tab][key]) {
       const aggregationArray = metaData[tab][key];
       const index = aggregationArray.indexOf(value);
@@ -107,14 +140,31 @@ function useMetaData() {
 
   useEffect(() => {
     if (!loading) {
-      updateFilteredList();
+      if (tab !== 'patient') {
+        updateFilteredList();
+      } else {
+        setFilteredList(['patient']);
+      }
     }
   }, [filter, tab, loading]);
 
+  function loadMetaData(obj) {
+    setMetaData(obj);
+    let num = 0;
+    Object.keys(obj).forEach((metaDataKey) => {
+      Object.keys(obj[metaDataKey]).forEach((feature) => {
+        num += obj[metaDataKey][feature].length;
+      });
+    });
+    updateBadgeNum(num);
+  }
+
   return {
     metaData,
+    setMetaData,
     updateMetaData,
     initialize,
+    initialized,
     loading,
     filter,
     updateFilter,
@@ -122,6 +172,9 @@ function useMetaData() {
     setTab,
     filteredList,
     badgeNum,
+    patientMetaData,
+    updateDate,
+    loadMetaData,
   };
 }
 
---
+++ ui/subComponents/metaDataBrowser/structuredData/MetaDataTable.jsx
@@ -0,0 +1,230 @@
+import React, { useState, useRef } from 'react';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import TableSortLabel from '@material-ui/core/TableSortLabel';
+import DateFnsUtils from '@date-io/date-fns';
+import { MuiPickersUtilsProvider, KeyboardDatePicker } from '@material-ui/pickers';
+import { Virtuoso } from 'react-virtuoso';
+import AutoSizer from 'react-virtualized-auto-sizer';
+import shortid from 'shortid';
+
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import CheckCircleIcon from '@material-ui/icons/CheckCircle';
+import CheckCircleOutlineIcon from '@material-ui/icons/CheckCircleOutline';
+import CheckIcon from '@material-ui/icons/Check';
+
+import './metaData.css';
+
+import prettyString from '../../../helperFunctions/prettyString';
+
+function MetaDataTable(props) {
+  const { metaData, numPatients, editable } = props;
+  const show = !metaData.loading;
+  const [expanded, setExpanded] = useState(-1);
+  const scrollPosition = useRef(null);
+
+  function updateExpanded(index) {
+    if (expanded === index) return setExpanded(-1);
+    return setExpanded(index);
+  }
+
+  function noData() {
+    return <div id="noMetaData">{`No ${metaData.tab} to show`}</div>;
+  }
+
+  function patientMetaDataList() {
+    const list = metaData.patientMetaData.map((row, index) => {
+      const selectedMetaData = metaData.metaData.patient[row.display] || [];
+      return (
+        <ExpansionPanel
+          key={shortid.generate()}
+          className="metaDataItem"
+          expanded={expanded === index}
+          onChange={() => updateExpanded(index)}
+        >
+          <ExpansionPanelSummary
+            expandIcon={<ExpandMoreIcon />}
+            className="metaDataItemTitle"
+          >
+            <div className="metaDataItemModified">
+              {selectedMetaData.length > 0 && (
+                <CheckIcon />
+              )}
+            </div>
+            <div className="metaDataDisplay">
+              {row.display}
+            </div>
+          </ExpansionPanelSummary>
+          <ExpansionPanelDetails className="metaDataPanel">
+            <div className="aggregationMethodButtons">
+              {row.display === 'Age' ? (
+                <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                  <KeyboardDatePicker
+                    disableToolbar
+                    variant="inline"
+                    // clearable
+                    format="MM/dd/yyyy"
+                    value={metaData.metaData.patient.date}
+                    onChange={(date) => metaData.updateDate(date)}
+                  />
+                  <Button
+                    variant="contained"
+                    className={selectedMetaData.indexOf('numeric') !== -1 ? 'checkedMetaData' : 'uncheckedMetaData'}
+                    endIcon={selectedMetaData.indexOf('numeric') !== -1 ? <CheckCircleIcon /> : <CheckCircleOutlineIcon />}
+                    onClick={() => metaData.updateMetaData(row.display, '', 'numeric')}
+                    disabled={!editable || !metaData.metaData.patient.date}
+                  >
+                    Numeric
+                  </Button>
+                  <Button
+                    variant="contained"
+                    className={selectedMetaData.indexOf('binned') !== -1 ? 'checkedMetaData' : 'uncheckedMetaData'}
+                    endIcon={selectedMetaData.indexOf('binned') !== -1 ? <CheckCircleIcon /> : <CheckCircleOutlineIcon />}
+                    onClick={() => metaData.updateMetaData(row.display, '', 'binned')}
+                    disabled={!editable || !metaData.metaData.patient.date}
+                  >
+                    Binned
+                  </Button>
+                </MuiPickersUtilsProvider>
+              ) : (
+                <Button
+                  variant="contained"
+                  className={selectedMetaData.indexOf('use') !== -1 ? 'checkedMetaData' : 'uncheckedMetaData'}
+                  endIcon={selectedMetaData.indexOf('use') !== -1 ? <CheckCircleIcon /> : <CheckCircleOutlineIcon />}
+                  onClick={() => metaData.updateMetaData(row.display, '', 'use')}
+                  disabled={!editable}
+                >
+                  Use It
+                </Button>
+              )}
+            </div>
+          </ExpansionPanelDetails>
+        </ExpansionPanel>
+      );
+    });
+    return list;
+  }
+
+  function generateItem(index) {
+    if (metaData.tab === 'patient') { // patient tab is special
+      return patientMetaDataList();
+    }
+    const item = metaData.filteredList[index];
+    const selectedMetaData = metaData.metaData[metaData.tab][`${item.code} ${item.system}`] || [];
+    return (
+      <ExpansionPanel
+        key={shortid.generate()}
+        className="metaDataItem"
+        expanded={expanded === index}
+        onChange={() => updateExpanded(index)}
+      >
+        <ExpansionPanelSummary
+          expandIcon={<ExpandMoreIcon />}
+          className="metaDataItemTitle"
+        >
+          <div className="metaDataItemModified">
+            {selectedMetaData.length > 0 && (
+              <CheckIcon />
+            )}
+          </div>
+          <div className="metaDataDisplay">
+            {item.display}
+          </div>
+          <div className="metaDataCoverage">
+            {(item.unique_count / numPatients).toPrecision(2)}
+          </div>
+        </ExpansionPanelSummary>
+        <ExpansionPanelDetails className="metaDataPanel">
+          <p>Aggregation Methods</p>
+          <div className="aggregationMethodButtons">
+            {metaData.filter[metaData.tab].features.map((feature) => (
+              <Button
+                key={shortid.generate()}
+                variant="contained"
+                className={selectedMetaData.indexOf(feature) !== -1 ? 'checkedMetaData' : 'uncheckedMetaData'}
+                endIcon={selectedMetaData.indexOf(feature) !== -1 ? <CheckCircleIcon /> : <CheckCircleOutlineIcon />}
+                onClick={() => metaData.updateMetaData(item.code, item.system, feature)}
+                disabled={!editable}
+              >
+                {prettyString(feature)}
+              </Button>
+            ))}
+          </div>
+        </ExpansionPanelDetails>
+      </ExpansionPanel>
+    );
+  }
+
+  return (
+    <div id="setupDataLeftTable">
+      <div id="setupDataLeftTab">
+        <List>
+          {Object.keys(metaData.filter).map((filterKey) => (
+            <ListItem
+              key={shortid.generate()}
+              button
+              onClick={() => {
+                metaData.setTab(filterKey);
+                setExpanded(-1);
+                scrollPosition.current.scrollToIndex({ index: 0 });
+              }}
+              className={metaData.tab === filterKey ? 'activeMetaDataTab' : ''}
+            >
+              <ListItemText primary={prettyString(filterKey)} />
+            </ListItem>
+          ))}
+        </List>
+      </div>
+      {show ? (
+        <div id="setupDataLeftContainer">
+          <div id="setupDataSortBar">
+            {metaData.tab !== 'patient' && (
+              <>
+                <div id="metaDataTextSearch">
+                  <TextField
+                    onChange={(e) => metaData.updateFilter('text', e.target.value)}
+                    margin="none"
+                    className="searchTextField metaDataSearch"
+                    inputProps={{ style: { padding: '3px 0px' } }}
+                    value={metaData.filter[metaData.tab].text}
+                  />
+                </div>
+                <TableSortLabel
+                  active
+                  direction={metaData.filter[metaData.tab].sort}
+                  onClick={() => metaData.updateFilter('sort')}
+                >
+                  Coverage
+                </TableSortLabel>
+              </>
+            )}
+          </div>
+          <div id="setupDataList">
+            <AutoSizer>
+              {({ width, height }) => (
+                <Virtuoso
+                  totalCount={metaData.filteredList.length || 1}
+                  overscan={50}
+                  style={{ height, width }}
+                  item={metaData.filteredList.length ? generateItem : noData}
+                  ref={scrollPosition}
+                />
+              )}
+            </AutoSizer>
+          </div>
+        </div>
+      ) : (
+        <CircularProgress size={100} thickness={2} />
+      )}
+    </div>
+  );
+}
+
+export default MetaDataTable;
